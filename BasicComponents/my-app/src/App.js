279a77b92a75f587c8e574734c54d5cb704d4dd8
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-29 11:06:43 +0300

    Created a react todo app

---
+++ BasicComponents/my-app/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo App</h1>
+      </header>
+
+      <main>
+    <h2>Tasks</h2>
+    <ul>
+      <li>Clean your room</li>
+      <li>Do exercise</li>
+      <li>Read a book</li>
+    </ul>
+      </main>
+
+      <footer>
+        <p>
+          All rights reserved &copy;
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;

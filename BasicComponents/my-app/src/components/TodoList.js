bde227c484078b3cb866e3aa2b8780eb1647c970
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-30 01:55:00 +0300

    Counter added.

--- BasicComponents/my-app/src/components/TodoList.js
+++ BasicComponents/my-app/src/components/TodoList.js
@@ -1,27 +1,44 @@
 import React, { useState} from 'react';
 import TodoListItem from './TodoListItem';
 
+// function TodoList() {
+//     // let todoState = React.useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book']);
+//     // let todos = todoState[0];
+//     // let setTodos = todoState[1];
+
+//     let [todos, setTodos] = useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book'])
+
+//     console.log(todos);
+
+//     return (
+
+//         <React.Fragment>
+//             <h2>Tasks</h2>
+//             <ul>
+//                 {todos.map(x => <TodoListItem text={x} />)}
+//             </ul>
+//             <button onClick={()=> setTodos([...todos, 'Modified'])}>Modify</button>
+//         </React.Fragment>
+
+//     )
+
+// }
+
+
 function TodoList() {
-    // let todoState = React.useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book']);
-    // let todos = todoState[0];
-    // let setTodos = todoState[1];
 
-    let [todos, setTodos] = useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book'])
-
-    console.log(todos);
+    let [count, setCount] = useState(0);
 
     return (
-
-        <React.Fragment>
-            <h2>Tasks</h2>
+        <>
+            <h2>Counter</h2>
             <ul>
-                {todos.map(x => <TodoListItem text={x} />)}
+                <TodoListItem text={count}></TodoListItem>
             </ul>
-            <button onClick={()=> setTodos([...todos, 'Modified'])}>Modify</button>
-        </React.Fragment>
+            <button onClick={()=> setCount(count + 1)}>+</button>
+        </>
 
     )
 
 }
-
 export default TodoList;

0a6e020817d7d4c9b5325e01dadf43f35b7188a0
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-29 13:06:55 +0300

    updated the state.

--- BasicComponents/my-app/src/components/TodoList.js
+++ BasicComponents/my-app/src/components/TodoList.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState} from 'react';
 import TodoListItem from './TodoListItem';
 
 function TodoList() {
-    let todoState = React.useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book']);
-    let todos = todoState[0];
-    let setTodos = todoState[1];
+    // let todoState = React.useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book']);
+    // let todos = todoState[0];
+    // let setTodos = todoState[1];
+
+    let [todos, setTodos] = useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book'])
 
     console.log(todos);
 

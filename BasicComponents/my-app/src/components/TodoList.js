50393e497f1dd94cbc02122a885151ae3499de4d
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-29 11:35:27 +0300

    fragment added

--- BasicComponents/my-app/src/components/TodoList.js
+++ BasicComponents/my-app/src/components/TodoList.js
@@ -1,10 +1,17 @@
+import React from 'react';
+
 function TodoList() {
     return (
+        // <>
+        <React.Fragment>
+        <h2>Tasks</h2>
         <ul>
             <li>Clean your room</li>
             <li>Do exercise</li>
             <li>Read a book</li>
         </ul>
+        </React.Fragment>
+        // </>
     )
 
 }

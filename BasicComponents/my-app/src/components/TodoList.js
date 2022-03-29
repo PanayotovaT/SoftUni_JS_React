13529d70be93210d6b53ddd056b1a8d124adaaf2
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-29 13:03:08 +0300

    TodoListItem added

--- BasicComponents/my-app/src/components/TodoList.js
+++ BasicComponents/my-app/src/components/TodoList.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import TodoListItem from './TodoListItem';
 
 function TodoList() {
+    let todoState = React.useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book']);
+    let todos = todoState[0];
+    let setTodos = todoState[1];
+
+    console.log(todos);
+
     return (
-        // <>
+
         <React.Fragment>
-        <h2>Tasks</h2>
-        <ul>
-            <li>Clean your room</li>
-            <li>Do exercise</li>
-            <li>Read a book</li>
-        </ul>
+            <h2>Tasks</h2>
+            <ul>
+                {todos.map(x => <TodoListItem text={x} />)}
+            </ul>
         </React.Fragment>
-        // </>
+
     )
 
 }

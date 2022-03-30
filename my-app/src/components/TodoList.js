841c3b69c81669dbab59ce0b6f41885e6d5c42d1
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-30 17:10:33 +0300

    Added state changes

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -1,26 +1,55 @@
 import React, { useState} from 'react';
 import TodoListItem from './TodoListItem';
 
+// function TodoList() {
+//     // let todoState = React.useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book']);
+//     // let todos = todoState[0];
+//     // let setTodos = todoState[1];
+
+//     let [todos, setTodos] = useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book'])
+
+//     console.log(todos);
+
+//     return (
+
+//         <React.Fragment>
+//             <h2>Tasks</h2>
+//             <ul>
+//                 {todos.map(x => <TodoListItem text={x} />)}
+//             </ul>
+//             <button onClick={()=> setTodos([...todos, 'Modified'])}>Modify</button>
+//         </React.Fragment>
+
+//     )
+
+// }
+
+
 function TodoList() {
-    // let todoState = React.useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book']);
-    // let todos = todoState[0];
-    // let setTodos = todoState[1];
 
-    let [todos, setTodos] = useState(['Clean your room', 'Go Shooping', 'Learn React', 'Read a book'])
+    let [count, setCount] = useState(0);
+    let [name, setName] = useState('');
 
-    console.log(todos);
+    const addButtonHandler = () => {
+        setCount(count + 1);
+        setName('Alex')
+    }
 
+    const inputChangeHandler = (e) => {
+        setName(e.target.value);
+    }
     return (
-
-        <React.Fragment>
-            <h2>Tasks</h2>
+        <>
+            <h2>Counter</h2>
             <ul>
-                {todos.map(x => <TodoListItem text={x} />)}
+                <TodoListItem text={count} name={name}></TodoListItem>
             </ul>
-        </React.Fragment>
+            <input type="text" onChange={inputChangeHandler} />
+            <button onClick={addButtonHandler}>+</button>
+            <button onClick={setCount.bind(null, count + 1)}>+</button>
+        </>
 
     )
 
 }
-
 export default TodoList;

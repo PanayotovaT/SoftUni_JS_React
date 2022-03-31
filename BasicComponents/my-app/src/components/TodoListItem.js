8aa291be068142f521bfd759f900ab79b5042284
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-03-31 11:38:24 +0300

    Adding all changes

--- BasicComponents/my-app/src/components/TodoListItem.js
+++ BasicComponents/my-app/src/components/TodoListItem.js
@@ -1,6 +1,11 @@
 const TodoListItem = (props) => {
+    if(props.count > 0) {
+         return (
+            <li style={{color: 'darkred'}}>{props.count} - {props.name}</li>
+         );
+    }
     return (
-        <li style={{color: props.color}}>{props.text} - {props.name}</li>
+        <li style={{color: props.color}}>{props.count} - {props.name}</li>
     );
 }
 

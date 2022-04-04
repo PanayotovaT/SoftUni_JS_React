61ea232082841d3279cb41b86e1b7507da434e98
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-04-04 12:19:57 +0300

    Added state and useEffect to show mounting.

--- Components_DeepDrive/todo-app/src/components/TodoList.js
+++ Components_DeepDrive/todo-app/src/components/TodoList.js
@@ -1,17 +1,38 @@
 // import uniqid from 'uniqid';
+import { useState, useEffect } from 'react';
 import TodoItem from "./TodoITem";
 
 export default function TodoList() {
+    const [todos, setTodos] = useState([
+        { id: 1, text: 'Clean my room' },
+        { id: 2, text: 'Learn React' },
+        { id: 3, text: 'Go shopping' },
+    ]);
 
-    const todos = [
-        {id:1,text: 'Clean my room'},
-        {id:2, text: 'Learn React'},
-       { id:3, text:'Go shopping'}
-    ]
+    useEffect(() => {
+        console.log('mounted')
+    }, []);
+
+  const onTodoInputBlur = (e) => {
+    let todo = {
+        id: todos.length + 1,
+        text: e.target.value
+
+    }
+
+    setTodos([
+        ...todos,
+        todo
+    ]);
+  }
     return (
-        <ul>
-           {todos.map(x => <TodoItem key={x.id} todo={x} />)} 
-        </ul>
+        <>
+        <label htmlFor="todo-name">Add Todo</label>
+        <input type="text" id="todo-name" onBlur={onTodoInputBlur} name="todo-name" />
+            <ul>
+                {todos.map(x => <TodoItem key={x.id} todo={x} />)}
+            </ul>
+        </>
     );
 
 }

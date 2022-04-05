f68482e39f32aba32eb5e54b9474eed0cd19cd8e
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-04-05 18:01:25 +0300

    Render, mounted, unmounted added.

--- Components_DeepDrive/todo-app/src/components/TodoITem.js
+++ Components_DeepDrive/todo-app/src/components/TodoITem.js
@@ -1,8 +1,16 @@
+import  { useEffect } from 'react';
+
 export default function TodoItem({
     todo,
     onDelete
 }) {
+    useEffect(() => {
+        console.log(todo.id, ' - Mounted');
 
+        return () => {
+            console.log('Unmounted - ', todo.id);
+        }
+    }, [])
     return ( 
         <li >{todo.text} <button onClick={()=> onDelete(todo.id)}>[X]</button></li>
     );

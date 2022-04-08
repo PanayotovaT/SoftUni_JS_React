cd8c1b264db4dbd0e1a4446b2d80c96d1a65ae96
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-04-08 16:12:42 +0300

    Fetched the game data.

--- WorkshopComponents/games-play/src/components/Catalog.js
+++ WorkshopComponents/games-play/src/components/Catalog.js
@@ -1,4 +1,13 @@
+import { useEffect, useState } from 'react';
+
 const Catalog = () => {
+    const [games, setGames] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+            .then(response =>  response.json())
+            .then(result => setGames(result));
+    }, []);
+
     return (
         <section id="catalog-page">
             <h1>All Games</h1>

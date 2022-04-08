6e90cfa2eeef30ed7207339e01cb0e141613c8d5
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-04-08 15:55:59 +0300

    Add error page.

--- WorkshopComponents/games-play/src/App.js
+++ WorkshopComponents/games-play/src/App.js
@@ -1,24 +1,39 @@
+import { useState } from "react";
+
 import Catalog from "./components/Catalog";
 import Create from "./components/Create";
 import Edit from "./components/Edit";
+import ErrorPage from "./components/ErrorPage";
 import GameDetails from "./components/GameDetails";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import WelcomeWorld from "./components/WelcomeWorld";
 
-function App() {
+function App() { 
+const [page, setPage] = useState('/home');
+
+const routes = {
+  '/home':  <WelcomeWorld />,
+  '/games': <Catalog />,
+  '/login':   <Login />,
+  '/register':   <Register />,
+  '/create':   <Create />,
+  '/edit':    <Edit />,
+  '/game-details':    <GameDetails />,
+}
+
+  const navigationChangeHandler = (path) => {
+    console.log(path);
+    setPage(path);
+
+  }
+
   return (
     <div id="box">
-      <Header />
+      <Header navigationChangeHandler={navigationChangeHandler} />
       <main id="main-content">
-        <WelcomeWorld />
-        <Login />
-        <Register />
-        <Create />
-        <Edit />
-        <GameDetails />
-        <Catalog />
+        {routes[page] || <ErrorPage />}
       </main>
 
     </div>

8d03b2a23abf0d37fe4f6639cf38e43538bb8ae9
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-04-16 16:56:06 +0300

    Uncontrolled form added.

--- Forms/demo-forms/src/App.js
+++ Forms/demo-forms/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   const submitHandler = (e) => {
     e.preventDefault();
+    console.log(e.currentTarget);
 
+    let formData = new FormData(e.currentTarget);
+    let username = formData.get('username');
+    let password = formData.get('password');
+
+    console.log(username);
+    console.log(password);
   }
 
   return (
@@ -11,11 +18,15 @@ function App() {
       <form method="POST" onSubmit={submitHandler}>
         <span className="form-span">
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
+          <input type="text" name="username" id="username" defaultValue="Alex" />
         </span>
         <span className="form-span">
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="assword" />
+        </span>
+        <span className="form-span" className="is-admin-span">
+          <label htmlFor="isAdmin" className="is-admin">isAdmin</label>
+          <input type="checkbox" name="isAdmin" id="isAdmin" defaultChecked />
         </span>
         <input type="submit" className="form-submit-btn" value="Submit" />
 

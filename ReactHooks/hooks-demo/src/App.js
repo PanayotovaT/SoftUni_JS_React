190303d5dba32423e876218aa0fc1f7dd3913bf8
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-04-19 23:57:30 +0300

    useEffect added.

--- ReactHooks/hooks-demo/src/App.js
+++ ReactHooks/hooks-demo/src/App.js
@@ -4,18 +4,31 @@ import './App.css';
 
 function App() {
 
-  const stateResult = useState('');
-  const name = stateResult[0];
-  const setName =stateResult[1]
+  // const stateResult = useState('');
+  // const name = stateResult[0];
+  // const setName =stateResult[1]
+  const [name, setName] = useState('');
+  const [count, setCount] = useState(0);
+  const [info, setInfo] = useState({
+    'name': 'Alex',
+    'age': 3,
+    'hobbies': ['first', 'second']
+  })
 
   useEffect(()=> {
     setTimeout(() => {
       setName('Alex');
+      setInfo(oldState => ({
+        ...oldState,
+        age: 4
+      }))
     },1500)
   }, [])
   return (
     <div className="App">
-        <h2>{name}</h2>
+        <h2>{!name ? 'Loading...': name}</h2>
+        <p>{count}</p>
+        <button onClick={() => setCount(x => x + 1)}>Count</button>
     </div>
   );
 }

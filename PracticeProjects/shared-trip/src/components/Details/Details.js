983379a96a041df0dc6d115d9ec09d4da6a472a8
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-04-27 11:29:48 +0300

    Details page added.

--- PracticeProjects/shared-trip/src/components/Details/Details.js
+++ PracticeProjects/shared-trip/src/components/Details/Details.js
@@ -1,18 +1,32 @@
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+import * as tripService from '../../services/tripService';
+
 
 
 const Details = () => {
+    const [trip, setTrip] =  useState({});
+    const id = useParams().carId;
+    console.log(id);
 
+    useEffect(() => {
+        tripService.getOne(id)
+            .then(trip => {
+                setTrip(trip);
+            })
+    }, [id])
     return (
         <section className="py-5 details" id="trip-details-page">
         <div className="container">
             <div className="destination">
                 <div>
                     <span className="icon"><i className="fas fa-map-marked-alt"></i></span>
-                    <h5> from <span>Plovdiv</span> to <span>Burgas</span> </h5>
+                    <h5> from <span>{trip.startPoint}</span> to <span>{trip.endPoint}</span> </h5>
                 </div>
                 <div>
                     <span className="icon"><i className="fas fa-calendar-alt"></i></span>
-                    <h5> <span>2021-07-07</span> at <span>10:40</span> </h5>
+                    <h5> <span>{trip.date}</span> at <span>{trip.time}</span> </h5>
                 </div>
             </div>
             <p className="line"></p>
@@ -24,27 +38,27 @@ const Details = () => {
 
                     <p>there are no buddies yet...</p>
                 </div>
-                <h5>Driver: <span>mihail_valkov@mail.bg</span> </h5>
+                <h5>Driver: <span>{trip.ownerId}</span> </h5>
             </div>
             <p className="line"></p>
-            <h5 className="brand">Car Brand: <span className="lead">Mercedes</span></h5>
+            <h5 className="brand">Car Brand: <span className="lead">{trip.brand}</span></h5>
             <div className="trip-info">
                 <div>
                     <img className="img-fluid rounded"
-                        src="https://www.nastarta.com/wp-content/uploads/2018/04/Mercedes-Benz-A-Class-2019-1280-3d.jpg"
+                        src={trip.imageUrl}
                         alt="car-image" />
                 </div>
                 <div className="trip-desc">
                     <h5>Information about the trip</h5>
                     <textarea className="lead"
-                        disabled>Wishing you a safe journey and a relaxing vacation when you arrive!</textarea>
-                    <h5>Price: <span className="lead">40</span> BGN</h5>
+                        disabled defaultValue={trip?.description} />
+                    <h5>Price: <span className="lead">{trip.price}</span> BGN</h5>
 
                     <div className="actions">
-                        <a href="" className="btn btn-danger">Delete this trip</a>
-                        <a href="" className="btn btn-warning">Edit this trip</a>
+                        <Link to={`/delete/${trip._id}`} className="btn btn-danger">Delete this trip</Link>
+                        <Link to={`/edit/${trip._id}`} className="btn btn-warning">Edit this trip</Link>
 
-                        <a href="" className="btn btn-join">Join now, [ 1 ] seats left!</a>
+                        <a href={`/join/${trip._id}`} className="btn btn-join">Join now, {trip.seats} seats left!</a>
 
                         <span className="btn btn-info">Already joined. Don't be late!</span>
 

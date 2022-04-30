1348ef504e4d840cd6a5f6436c41519e42ba1954
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-04-30 03:32:16 +0300

    Added reducer.

--- ReactAdvancedTechniques/advanced-techniques-demo/src/contexts/AuthContext.js
+++ ReactAdvancedTechniques/advanced-techniques-demo/src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, useReducer } from 'react';
 
 export const AuthContext = createContext();
 
@@ -6,21 +6,44 @@ const initialState = {
     email: '',
 }
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return { ...state, email: action.payload };
+        case 'LOGOUT' :
+            return initialState;
+        default:
+            return state;
+    }
+
+}
+
 export const AuthProvider = ({
     children
 }) => {
-    const [user, setUser] = useState(initialState);
+    // const [user, setUser] = useState(initialState);
+    const [user, dispatch] = useReducer(reducer, initialState);
 
     const login = (email) => {
-        setUser({
-            email,
+        // setUser({
+        //     email,
+        // })
+        dispatch({
+            type: 'LOGIN',
+            payload: email
+        });
+    }
+
+    const logout = () => {
+        dispatch({
+            type: 'LOGOUT',
         })
     }
 
     const isAuthenticated = Boolean(user.email);
 
     return (
-        <AuthContext.Provider value={{user, login, isAuthenticated}}>
+        <AuthContext.Provider value={{ user, login, logout, isAuthenticated }}>
             {children}
         </AuthContext.Provider>
     )

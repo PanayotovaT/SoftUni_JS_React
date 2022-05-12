d1665d80d120a563e40b920e5a40bf784496c3e3
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-05-12 18:04:47 +0300

    Starting a new project.

---
+++ PracticeProjects/wildlife-photography/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+
+    <div className="site-wrapper">
+      <Header />
+    <main>
+      
+    </main>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ PracticeProjects/wildlife-photography/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+
+const Header = () => {
+
+    return (
+        <nav>
+        <img src="/img/logo.png" alt="logo" />
+
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">All Posts</a></li>
+            <li><a href="#">Create Post</a></li>
+            <li><a href="#">Posts of [email]</a></li>
+            <li><a href="#">Logout</a></li>
+            <li><a href="#">Register</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+
+    </nav>
+    );
+}
+
+export default Header;

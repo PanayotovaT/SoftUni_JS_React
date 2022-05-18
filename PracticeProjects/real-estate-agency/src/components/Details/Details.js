f1923de6d25d0384a2816a4cbce9d035324e414a
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-05-18 12:32:33 +0300

    Visibility of details updated.

--- PracticeProjects/real-estate-agency/src/components/Details/Details.js
+++ PracticeProjects/real-estate-agency/src/components/Details/Details.js
@@ -1,44 +1,65 @@
 import { Link, useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 import useEstate from '../../hooks/useEstate';
+import { useAuthContext } from '../../contexts/AuthContext';
 
 const Details = () => {
     const { estateId } = useParams()
-    const [estate ] = useEstate(estateId);
+    const [estate] = useEstate(estateId);
+    const { user } = useAuthContext();
+    const [rents, setSents] = useState([]);
+
+    const ownerButtons = (
+        <>
+            <Link to="/edit/123" className="edit">Edit</Link>
+            <Link to="/delete/123" className="remove">Delete</Link>
+        </>
+    );
+
+    const userButtons = (
+        <>
+            <Link to="/rent/123" className="rentHome">Rent a home, available 2 housing</Link>
+            <p className="alRentHome">You have already rent this home</p>
+
+            <p className="no-housing">No Housing Available!</p>
+        </>
+    )
 
     return (
         <section id="deatils-page">
-        <div className="wrapper">
-            <div className="product-img">
-                <img src={estate.imageUrl} alt="appartments" />
-            </div>
-            <div className="product-info">
-                <div className="product-text">
+            <div className="wrapper">
+                <div className="product-img">
+                    <img src={estate.imageUrl} alt="appartments" />
+                </div>
+                <div className="product-info">
                     <div className="product-text">
-                        <h1>Name: {estate.name}</h1>
-                        <h2>Type: {estate.type}</h2>
-                        <h4>Year: {estate.year}</h4>
-                        <h4>City: {estate.city}</h4>
-                        <p>Description: {estate.description}</p>
+                        <div className="product-text">
+                            <h1>Name: {estate.name}</h1>
+                            <h2>Type: {estate.type}</h2>
+                            <h4>Year: {estate.year}</h4>
+                            <h4>City: {estate.city}</h4>
+                            <p>Description: {estate.description}</p>
+                            {rents.length > 0 
+                                ? <p>People rented this housing: {rents.map(x => x.name).join(', ')}</p>
+                                : <p>People rented this housing: There are no tenants yet.</p>
+                            }
+                            
 
-                        <p>People rented this housing: Alex Petkov, Ivan Dobrev</p>
+                            
+                        </div>
+                    </div>
 
-                        <p>People rented this housing: There are no tenants yet.</p>
+                    <div className="product-btn">
+                        {user._id == estate._ownerId
+                            ? ownerButtons
+                            : userButtons
+                        }
+
                     </div>
                 </div>
-
-                <div className="product-btn">
-                    <Link to="/edit/123" className="edit">Edit</Link>
-                    <Link to="/delete/123" className="remove">Delete</Link>
-                    
-                    <Link to="/rent/123" className="rentHome">Rent a home, available 2 housing</Link>
-                    <p className="alRentHome">You have already rent this home</p>
-
-                    <p className="no-housing">No Housing Available!</p>
-                </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 }
 

c79b9432b4bee5cfec05e3a2522533e07205a2e8
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-06-11 15:47:03 +0300

    Added css for the active links.

--- react-library/src/components/Header/Header.js
+++ react-library/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './Header.css';
 
@@ -7,13 +7,13 @@ const Header = () => {
     return (
         <header className="header">
             <ul className="header-list">
-                <li className="header-list-item"><Link to="/" className="header-list-item-link">Home</Link></li>
-                <li className="header-list-item"><Link to="/dashboard" className="header-list-item-link">Dashboard</Link></li>
-                <li className="header-list-item"><Link to="/categories" className="header-list-item-link">Categories</Link></li>
-                <li className="header-list-item"><Link to="/my-books" className="header-list-item-link">My books</Link></li>
-                <li className="header-list-item"><Link to="/login" className="header-list-item-link">Login</Link></li>
-                <li className="header-list-item"><Link to="/register" className="header-list-item-link">Register</Link></li>
-                <li className="header-list-item"><Link to="/logout" className="header-list-item-link">Logout</Link></li>
+                <li className="header-list-item"><NavLink to="/" className={({isActive})=> isActive ? 'active-header-link' : 'header-list-item-link'}>Home</NavLink></li>
+                <li className="header-list-item"><NavLink to="/dashboard" className={({isActive})=> isActive ? 'active-header-link' : 'header-list-item-link'}>Dashboard</NavLink></li>
+                <li className="header-list-item"><NavLink to="/categories" className={({isActive})=> isActive ? 'active-header-link' : 'header-list-item-link'}>Categories</NavLink></li>
+                <li className="header-list-item"><NavLink to="/my-books" className={({isActive})=> isActive ? 'active-header-link' : 'header-list-item-link'}>My books</NavLink></li>
+                <li className="header-list-item"><NavLink to="/login" className={({isActive})=> isActive ? 'active-header-link' : 'header-list-item-link'}>Login</NavLink></li>
+                <li className="header-list-item"><NavLink to="/register" className={({isActive})=> isActive ? 'active-header-link' : 'header-list-item-link'}>Register</NavLink></li>
+                <li className="header-list-item"><NavLink to="/logout" className={({isActive})=> isActive ? 'active-header-link' : 'header-list-item-link'}>Logout</NavLink></li>
             </ul>
 
         </header>

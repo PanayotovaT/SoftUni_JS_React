1038f970bdaf1075222dc2eecf3975263408167b
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-06-12 23:40:44 +0300

    Added authService register.

---
+++ react-library/src/services/authService.js
@@ -0,0 +1,35 @@
+const baseUrl = 'http://localhost:3030/users';
+
+export const register = async (email, gender, password) => {
+    try {
+        let response = await fetch(`${baseUrl}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, gender, password })
+        });
+        const result = await response.json();
+        if (result.ok) {
+            return result;
+        } else {
+            throw result.message;
+        }
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+
+}
+
+export const login = (email, password) => {
+
+}
+
+export const getUser = () => localStorage.getItem('username')
+
+export const isAuthenticated = () => Boolean(getUser());
+
+export const logout = () => {
+
+}

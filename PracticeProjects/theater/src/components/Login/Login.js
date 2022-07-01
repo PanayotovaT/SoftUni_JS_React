8bfcf415796cb78bb4f5caea0d63eecf2674c92a
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-07-02 01:41:17 +0300

    Login functionality added.

--- PracticeProjects/theater/src/components/Login/Login.js
+++ PracticeProjects/theater/src/components/Login/Login.js
@@ -1,11 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../../contexts/AuthContext';
+import * as authService from '../../services/authService';
+
 const Login = () => {
+    const navigate = useNavigate();
+    const { login } = useAuthContext();
+
+    const loginHandler = (e) =>{
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+
+        const email =  formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        if(email == '' || password == '') {
+            console.error('All fields are requested');
+            return;
+        }
+
+        authService.login(email, password)
+            .then(data => {
+                login(data);
+                navigate('/');
+            }).catch(err => {
+                console.error(err);
+                return;
+            })
+
+    }
 
     return (
-        <form  method="POST">
+        <form  method="POST" onSubmit={loginHandler}>
             <h2>Login</h2>
             <div>
                 <label htmlFor="username">Username:</label>
-                <input type="text" placeholder="Username" name="username" />
+                <input type="text" placeholder="Username" name="email" />
             </div>
             <div>
                 <label htmlFor="password">Password:</label>
--- PracticeProjects/theater/src/services/authService.js
+++ PracticeProjects/theater/src/services/authService.js
@@ -2,7 +2,7 @@ import { get, post } from "./requesterer";
 
 const baseUrl = 'http://localhost:3030/users';
 
-export const login = async (username, password) => await post(`${baseUrl}/login`, {username, password});
+export const login = async (email, password) => await post(`${baseUrl}/login`, {email, password});
 
 
 export const register  = async (email, password) => await post(`${baseUrl}/register`, {email, password});

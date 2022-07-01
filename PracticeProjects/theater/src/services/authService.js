f5abb5a8e1aa479dc0d77bf55f595bec91e70859
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-07-01 23:41:51 +0300

    authService register and login added.

--- PracticeProjects/theater/src/services/authService.js
+++ PracticeProjects/theater/src/services/authService.js
@@ -1,8 +1,9 @@
-export const login = async (username, password) => {
+import { post } from "./requesterer";
 
-}
+const baseUrl = 'http://localhost:3030/users';
 
-export const register  = async (username, password) => {
+export const login = async (username, password) => await post(`${baseUrl}/login`, {username, password});
 
-}
+
+export const register  = async (username, password) => await post(`${baseUrl}/register`, {username, password});
 

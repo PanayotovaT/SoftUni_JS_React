bc2aca344eb908727493b42250db4b89e0e96ad0
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-07-03 23:52:08 +0300

    Button visibility in the Details section added.

--- PracticeProjects/theater/src/components/Details/Details.js
+++ PracticeProjects/theater/src/components/Details/Details.js
@@ -2,16 +2,20 @@ import { Link } from "react-router-dom";
 
 import './Details.css';
 import useFilm from "../../hooks/useFilm";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const Details = () => {
     const [film] = useFilm();
+    const { isAuthenticated, user } = useAuthContext();
+
+    const isOwner = user._id == film._ownerId;
 
     return (
         <div className="container">
 
-            <section className="details">
+            <section className="details details-section">
                 <div className="details-image">
-                    <h1>Theater title: {film.title}</h1>
+                    <h1 className="details-headline">Theater title: {film.title}</h1>
                     <div>
                         <img src={film.imageUrl} alt="random" className="details-img" />
                     </div>
@@ -19,14 +23,26 @@ const Details = () => {
                 </div>
                 <div className="details-text">
 
-                    <h3>Theater Description</h3>
+                    <h3 className="details-headline" >Theater Description</h3>
                     <p>{film.description}</p>
-                    <div className="buttons">
-                        <Link className="btn delete" to={`/delete/${film._id}`}>Delete</Link>
-                        <Link className="btn edit" to={`/edit/${film._id}`}>Edit</Link>
-                        <span className="liked">You have already liked this play!</span>
-                        <Link className="btn like" to={`/like/${film._id}`}>Like</Link>
-                    </div>
+                    {isAuthenticated
+                        ? (<div className="details-buttons buttons">
+                            {isOwner
+                                ? (<>
+                                    <Link className="btn delete details-btn" to={`/delete/${film._id}`} >Delete</Link>
+                                    <Link className="btn edit details-btn" to={`/edit/${film._id}`} >Edit</Link>
+                                </>)
+                                : (<>
+                                    <span className="liked">You have already liked this play!</span>
+                                    <Link className="btn like details-btn" to={`/like/${film._id}`} >Like</Link>
+                                </>)
+                            }
+
+
+                        </div>)
+                        : ''
+                    }
+
                 </div>
 
 

3beeb52545fba404dad02305d0ac212df20715d0
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-07-04 03:15:22 +0300

    Added the deleteDunctionality.

--- PracticeProjects/theater/src/components/Details/Details.js
+++ PracticeProjects/theater/src/components/Details/Details.js
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 import './Details.css';
 import useFilm from "../../hooks/useFilm";
 import { useAuthContext } from "../../contexts/AuthContext";
+import { deleteFilm } from "../../services/filmService";
 
 const Details = () => {
     const [film] = useFilm();
     const { isAuthenticated, user } = useAuthContext();
+    const { id } = useParams();
+    const navigate = useNavigate();
 
     const isOwner = user._id == film._ownerId;
+
+    const deleteHandler = (e) => {
+        e.preventDefault();
+        deleteFilm(id)
+            .then(res => {
+                navigate('/');
+            })
+            .catch(err => {
+                return;
+            })
+
+    }
 
     return (
         <div className="container">
@@ -29,7 +44,7 @@ const Details = () => {
                         ? (<div className="details-buttons buttons">
                             {isOwner
                                 ? (<>
-                                    <Link className="btn delete details-btn" to={`/delete/${film._id}`} >Delete</Link>
+                                    <Link className="btn delete details-btn" to={`/delete/${film._id}`} onClick={deleteHandler} >Delete</Link>
                                     <Link className="btn edit details-btn" to={`/edit/${film._id}`} >Edit</Link>
                                 </>)
                                 : (<>
--- PracticeProjects/theater/src/components/Header/Header.js
+++ PracticeProjects/theater/src/components/Header/Header.js
@@ -1,21 +1,38 @@
 import { NavLink } from "react-router-dom";
-
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const Header = () => {
+    const { isAuthenticated } = useAuthContext();
+
+    const guestLinks = (
+        <>
+            <li><NavLink to="/login">Login</NavLink></li>
+            <li><NavLink to="/register">Register</NavLink></li>
+        </>
+    );
+
+    const userLinks = (
+        <>
+            <li><NavLink to="/create">Create </NavLink></li>
+            <li><NavLink to="/profile">My Films </NavLink></li>
+            <li><NavLink to="logout">Logout</NavLink></li>
+        </>
+    );
 
     return (
-   
-            <nav>
-                <NavLink to="/catalog">Theater</NavLink>
-                <ul>
-                    <li><NavLink to="/">Home </NavLink></li>
-                    <li><NavLink to="/create">Create </NavLink></li>
-                    <li><NavLink to="/catalog">Catalog </NavLink></li>
-                    <li><NavLink to="logout">Logout</NavLink></li>
-                    <li><NavLink to="/login">Login</NavLink></li>
-                    <li><NavLink to="/register">Register</NavLink></li>
-                </ul>
-            </nav>
+
+        <nav>
+            <NavLink to="/catalog">Theater</NavLink>
+            <ul>
+                <li><NavLink to="/">Home </NavLink></li>
+                <li><NavLink to="/catalog">Catalog </NavLink></li>
+                {isAuthenticated 
+                    ? userLinks
+                    : guestLinks
+                }
+
+            </ul>
+        </nav>
 
     );
 }
